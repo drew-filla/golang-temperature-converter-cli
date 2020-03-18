af9683e6d77c06f5e22693781cdf20c61170b59f
Filla <AJF0629@daugherty.com>
2020-03-18 15:40:30 -0500

    fixed os args

--- main.go
+++ main.go
@@ -18,6 +18,12 @@ var errReadingInput = errors.New("Error reading input")
 
 func main() {
 
+	if len(os.Args) != 2 {
+		printError(errInvalidArguments)
+	}
+
+	originUnit = string.ToUpper(os.Args[1])
+
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
 
