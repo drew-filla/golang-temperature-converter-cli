231fd438692849c7aefc41e635a251e81298a885
Filla <AJF0629@daugherty.com>
2020-03-18 15:44:41 -0500

    fixed string -> strings for ToUpper

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var originUnit string
@@ -22,7 +23,7 @@ func main() {
 		printError(errInvalidArguments)
 	}
 
-	originUnit = string.ToUpper(os.Args[1])
+	originUnit = strings.ToUpper(os.Args[1])
 
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
